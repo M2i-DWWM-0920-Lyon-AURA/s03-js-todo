2342580d9725fac5a1e59093f2c271cb358649fd
Romain Tello <romain.tello@gmail.com>
2020-09-29 16:39:09 +0200

    Refactor app using jQuery

--- app.js
+++ app.js
@@ -13,48 +13,49 @@ En JS 6, le mot-clé var est obsolète et est remplacé par const et let:
 */
 const app = {
   // Sélectionne le formulaire "Add todo"
-  addTodoForm: document.getElementById('add-todo'),
+  addTodoForm: $('#add-todo'),
   // Sélectionne le champ texte
-  todoNameInput: document.getElementById('add-todo-name'),
+  todoNameInput: $('#add-todo-name'),
   // Sélectionne le champ texte
-  addTodoButton: document.getElementById('add-todo-button'),
+  addTodoButton: $('#add-todo-button'),
   // Sélectionne la liste des tâches à faire
-  todoList: document.getElementById('todo-list'),
+  todoList: $('#todo-list'),
 
   // Initialise l'application
   init: function() {
     // Associe une action au fait de valider le formulaire "Add todo"
-    app.addTodoForm.addEventListener(
+    app.addTodoForm.on(
       'submit',
       function(event) {
         // Empêche le rechargement de la page
         // (comportement normal du fait de valider un formulaire)
         event.preventDefault();
         // Récupère le contenu du champ texte
-        const todoName = app.todoNameInput.value;
+        const todoName = app.todoNameInput.val();
         // Efface le contenu du champ texte
-        app.todoNameInput.value = '';
+        app.todoNameInput.val('');
         // Désactive le bouton
-        app.addTodoButton.disabled = true;
+        app.addTodoButton.attr('disabled', true);
         // Ajoute une nouvelle tâche
         app.addTodo(todoName);
       }
     );
     // Associe une action au fait que la valeur du champ texte soit modifiée
-    app.todoNameInput.addEventListener(
+    app.todoNameInput.on(
       'input',
       function(event) {
         // Si le champ texte est vide
         if (event.target.value === '') {
           // Désactive le bouton
-          app.addTodoButton.disabled = true;
+          app.addTodoButton.attr('disabled', true);
           // Sinon
         } else {
           // Enlève le statut désactivé du bouton
-          app.addTodoButton.disabled = false;
+          app.addTodoButton.attr('disabled', false);
         }
       }
     );
+
     // Ajoute des tâches par défaut
     app.addTodo('Acheter des bananes');
     app.addTodo('Ranger ma chambre');
@@ -64,114 +65,80 @@ const app = {
   // Ajoute une nouvelle tâche à faire dans la liste
   addTodo: function(todoName) {
     // Crée un nouvel élément de liste
-    const newTodo = document.createElement('li');
-    newTodo.className = 'todo list-group-item d-flex align-items-center justify-content-between';
-    // Crée un nouveau élément texte
-    const todoNameElement = document.createElement('h2');
-    todoNameElement.className = 'todo-name';
-    todoNameElement.innerText = todoName;
-    // Ajoute cet élément texte à l'élément de liste
-    newTodo.appendChild(todoNameElement);
+    const newTodo = $('<li class="todo list-group-item d-flex align-items-center justify-content-between"><h2 class="todo-name"></h2><input type="checkbox" class="todo-toggle order-first" /><form class="todo-edit-name d-none"><input type="text" class="todo-edit-name-input" /><button class="btn btn-outline-success btn-sm"><i class="fas fa-check"></i></button></form><div class="todo-buttons"><button class="todo-buttons-edit btn btn-outline-warning btn-sm"><i class="fas fa-edit"></i></button><button class="todo-buttons-delete btn btn-outline-danger btn-sm"><i class="fas fa-trash-alt"></i></button></div></li>');
     // Ajoute cet élément de liste dans la liste
-    app.todoList.appendChild(newTodo);
-    // =================================================================================
-    // Crée une checkbox
-    const checkBox = document.createElement('input');
-    checkBox.className = 'order-first';
-    checkBox.type = 'checkbox';
+    $('#todo-list').append(newTodo);
+    // Ajoute le nom de la tâche à faire
+    newTodo.find('.todo-name').html(todoName);
+
+    // Sélectionne la checkbox
+    const checkbox = newTodo.find('.todo-toggle');
     // Associe une action au fait de cocher la checkbox
-    checkBox.addEventListener(
+    checkbox.on(
       'change',
       function(event) {
         // Si la checkbox vient d'être cochée
         if (event.target.checked) {
           // Rajouter une classe "done" afin de faire apparaître
           // la tâche comme terminée
-          newTodo.classList.add('done');
-        // Sinon
+          newTodo.addClass('done');
+          // Sinon
         } else {
           // Enlever la classe "done" afin de faire apparaître
           // la tâche comme non terminée
-          newTodo.classList.remove('done');
+          newTodo.removeClass('done');
         }
       }
     );
-    // Ajoute la checkbox au nouvel élément de liste
-    newTodo.appendChild(checkBox);
-    // =================================================================================
-    // Crée un formulaire permettant de modifier le nom de la tâche
-    const nameEdit = document.createElement('form');
-    // Rend le formulaire invisible par défaut
-    nameEdit.className = 'd-none';
-    // Crée un champ texte
-    const nameEditInput = document.createElement('input');
-    nameEditInput.type = 'text';
-    // Ajoute le champ texte au formulaire
-    nameEdit.appendChild(nameEditInput);
-    // Crée un bouton permettant de valider le formulaire
-    const nameEditButton = document.createElement('button');
-    nameEditButton.className = 'btn btn-outline-success btn-sm';
-    nameEditButton.innerHTML = '<i class="fas fa-check"></i>';
-    // Ajoute le bouton au formulaire
-    nameEdit.appendChild(nameEditButton);
-    // Ajoute le formulaire au nouvel élément de liste
-    newTodo.appendChild(nameEdit);
-    // Associe une action au fait de valider le formulaire
-    nameEdit.addEventListener(
+
+    // Sélectionne l'élément de texte
+    const todoNameElement = newTodo.find('.todo-name');
+    // Sélectionne le formulaire de changememt de nom
+    const nameEdit = newTodo.find('.todo-edit-name');
+    // Associe une action au fait de valider le formulaire de changement de nom
+    nameEdit.on(
       'submit',
       function(event) {
         // Empêche le rechargement de la page
         // (comportement normal du fait de valider un formulaire)
         event.preventDefault();
         // Récupère la valeur du champ texte
-        const newName = nameEditInput.value;
+        const newName = nameEdit.find('.todo-edit-name-input').val();
         // Remplace le texte du nouvel élément de liste par ce texte
-        todoNameElement.innerText = newName;
+        todoNameElement.text(newName);
         // Masque le formulaire
-        nameEdit.classList.add('d-none');
+        nameEdit.addClass('d-none');
         // Affiche l'élément de texte
-        todoNameElement.classList.remove('d-none');
+        todoNameElement.removeClass('d-none');
       }
     );
-    // =================================================================================
-    // Crée un bloc de boutons pour accueillir les boutons "Modifier" et "Supprimer"
-    const buttonBlock = document.createElement('div');
-    newTodo.appendChild(buttonBlock);
-    // =================================================================================
-    // Crée un bouton "Modifier" permettant d'afficher le formulaire de changement de nom
-    const editButton = document.createElement('button');
-    editButton.innerHTML = '<i class="fas fa-edit"></i>';
-    editButton.className = 'btn btn-outline-warning btn-sm';
+
+    // Sélectionne le bouton "Modifier"
+    const editButton = newTodo.find('.todo-buttons-edit');
     // Associe une action au fait de cliquer sur le bouton "Modifier"
-    editButton.addEventListener(
+    editButton.on(
       'click',
       function() {
         // Masquer l'élément de texte
-        todoNameElement.classList.add('d-none');
+        todoNameElement.addClass('d-none');
         // Afficher le formulaire de changement de nom
-        nameEdit.classList.remove('d-none');
+        nameEdit.removeClass('d-none');
         // Donne au champ texte du formulaire la valeur actuelle de l'élément de texte
-        nameEditInput.value = todoNameElement.innerText;
+        const nameInput = newTodo.find('.todo-edit-name-input');
+        nameInput.val(todoNameElement.text());
       }
     );
-    // Ajoute le bouton "Modifier" au nouvel élément de liste
-    buttonBlock.appendChild(editButton);
-      
-    // =================================================================================
-    // Crée un nouveau bouton "Supprimer"
-    const deleteButton = document.createElement('button');
-    deleteButton.className = 'btn btn-outline-danger btn-sm';
-    deleteButton.innerHTML = '<i class="fas fa-trash-alt"></i>';
+
+    // Sélectionne le bouton "Supprimer"
+    const deleteButton = newTodo.find('.todo-buttons-delete');
     // Associe une action au fait de cliquer sur le bouton "Supprimer"
-    deleteButton.addEventListener(
+    deleteButton.on(
       'click',
       function() {
         // Supprime l'élément de la liste
-        app.todoList.removeChild(newTodo);
+        newTodo.remove();
       }
     );
-    // Ajoute ce bouton au nouvel élément de liste
-    buttonBlock.appendChild(deleteButton);
   },
 };
 
